acdd7abc2df7ea276dd023ecc4819c2d3726d090
SAMMY HASS <sammyhass@live.co.uk>
2016-04-28 13:15:28 +0100

    Changed JS as age would be wrong after August

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var date= new Date();
 var year = date.getFullYear();
 htmlYear = document.getElementById("year");
 htmlYear.innerHTML = year;
-if (date.getMonth() < 7) {
+if (date.getMonth() < 6) { // Get Month starts at index 0.
 	year -= 1;
 } 
 var currentAge = year - 2001;
