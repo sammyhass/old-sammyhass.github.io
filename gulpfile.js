3ed84a00ba31ccfd2d813f1340702729d8dee30e
SAMMY HASS <sammyhass@live.co.uk>
2016-07-26 10:12:52 +0100

    Index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp"),
 	sass = require("gulp-sass"),
-	concat = require("gulp-concat");
+	concat = require("gulp-concat"),
+	cleanCSS = require("gulp-clean-css");
+
 
 gulp.task("sass", function () {
 	return gulp.src("./sass/*.scss")
@@ -12,10 +14,16 @@ gulp.task("sass:watch", function () {
 	gulp.watch("./sass/**/*.scss", ["sass"]);
 });
 
+gulp.task("minify-css", function () {
+	return gulp.src("css/*.css")
+		.pipe(cleanCSS({compatibility: "ie8"}))
+		.pipe(gulp.dest("css"))
+});
+
 // gulp.task("scripts", function () {
 // 	return gulp.src("./js/*.js")
 // 		.pipe(concat("app.js"))
 // 		.pipe(gulp.dest("./js/"))
 // });
 
-gulp.task("default", ["sass:watch"]);
+gulp.task("default", ["sass:watch", "minify-css"]);
