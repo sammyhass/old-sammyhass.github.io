2bd873ded65a91b10fbdad5574723bf08906b9b1
SAMMY HASS <sammyhass@live.co.uk>
2016-08-05 17:46:57 +0100

    New design

--- js/my.js
+++ js/my.js
@@ -1,15 +1,7 @@
-var jobs = [
-	"Lion Tamer",
-	"Llama Aprecciator",
-	"Professional Lad",
-	"Pencil Architect",
-	"Flying Unicorn"
-]
-var _ = document.querySelector.bind(document);
-var desc = _("#desc");
-var rand;
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	scrollBar: true
+    });
 
-setInterval(function () {
-	rand = Math.floor(Math.random() * jobs.length);
-	desc.innerHTML = jobs[rand];
-}, 3000);
+
+});
